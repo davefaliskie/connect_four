6d7f77c80f58655665bdfe0c43bb6e1086e297f6
Dave Faliskie <davefaliskie@gmail.com>
2022-07-05 22:50:05 -0500

    add all 4 win state functions

--- lib/src/game_internals/board_state.dart
+++ lib/src/game_internals/board_state.dart
@@ -111,6 +111,25 @@ class BoardState extends ChangeNotifier {
       winTiles = vertical;
       return true;
     }
+
+    List<Tile>? horizontal = horizontalCheck(playTile, takenTiles);
+    if (horizontal != null) {
+      winTiles = horizontal;
+      return true;
+    }
+
+    List<Tile>? forwardDiagonal = forwardDiagonalCheck(playTile, takenTiles);
+    if (forwardDiagonal != null) {
+      winTiles = forwardDiagonal;
+      return true;
+    }
+
+    List<Tile>? backDiagonal = backDiagonalCheck(playTile, takenTiles);
+    if (backDiagonal != null) {
+      winTiles = backDiagonal;
+      return true;
+    }
+
     return false;
   }
 
@@ -133,6 +152,125 @@ class BoardState extends ChangeNotifier {
     return null;
   }
 
+  List<Tile>? horizontalCheck(Tile playTile, List<Tile> takenTiles) {
+    // add the play tile to the list
+    List<Tile> tempWinTiles = [playTile];
 
+    // Look left, unless playTile is the first tile.
+    // Start at playTile.col - 1
+    if (playTile.col > 1) {
+      for (var col = playTile.col - 1; col > 0; col--) {
+        Tile tile = Tile(col: col, row: playTile.row);
+
+        if (takenTiles.contains(tile)) {
+          tempWinTiles.add(tile);
+        } else {
+          break;
+        }
+      }
+    }
+
+    // Look right, unless playTile is the last tile.
+    // Start at playTile.col + 1
+    if (playTile.col < boardSetting.cols) {
+      for (var col = playTile.col + 1; col < boardSetting.cols + 1; col++) {
+        Tile tile = Tile(col: col, row: playTile.row);
+
+        if (takenTiles.contains(tile)) {
+          tempWinTiles.add(tile);
+        } else {
+          break;
+        }
+      }
+    }
+
+    // see if tempWinTiles meets the win condition, if so it's a win
+    if (tempWinTiles.length >= boardSetting.winCondition()) {
+      return tempWinTiles;
+    }
+
+    return null;
+  }
+
+  List<Tile>? forwardDiagonalCheck(Tile playTile, List<Tile> takenTiles) {
+    // add the play tile to the list
+    List<Tile> tempWinTiles = [playTile];
+
+    // Look left & down, unless playTile is the first tile or in row 1.
+    // Start at playTile.col - 1
+    if (playTile.col > 1 && playTile.row > 1) {
+      // iterate to check all lower rows
+      for (var i = 1; i < playTile.row + 1; i++) {
+        Tile tile = Tile(col: playTile.col - i, row: playTile.row - i);
+
+        if (takenTiles.contains(tile)) {
+          tempWinTiles.add(tile);
+        } else {
+          break;
+        }
+      }
+    }
+
+    // Look right & up, unless playTile is the last tile or in top row.
+    // Start at playTile.col - 1
+    if (playTile.col < boardSetting.cols && playTile.row < boardSetting.rows) {
+      // iterate to check all upper rows. loop until hitting the top.
+      // so from (top - playTile.row) times.
+      for (var i = 1; i < (boardSetting.rows + 1) - playTile.row; i++) {
+        Tile tile = Tile(col: playTile.col + i, row: playTile.row + i);
+        if (takenTiles.contains(tile)) {
+          tempWinTiles.add(tile);
+        } else {
+          break;
+        }
+      }
+    }
+
+    // see if tempWinTiles meets the win condition, if so it's a win
+    if (tempWinTiles.length >= boardSetting.winCondition()) {
+      return tempWinTiles;
+    }
+
+    return null;
+  }
+
+  List<Tile>? backDiagonalCheck(Tile playTile, List<Tile> takenTiles) {
+    // add the play tile to the list
+    List<Tile> tempWinTiles = [playTile];
+
+    // Look left & up, unless playTile is the first tile or in top row.
+    if (playTile.col > 1 && playTile.row < boardSetting.rows) {
+      // iterate to check all upper rows
+      for (var i = 1; i < (boardSetting.rows + 1) - playTile.row; i++) {
+        Tile tile = Tile(col: playTile.col - i, row: playTile.row + i);
+
+        if (takenTiles.contains(tile)) {
+          tempWinTiles.add(tile);
+        } else {
+          break;
+        }
+      }
+    }
+
+    // Look right & down, unless playTile is the last tile or bottom row.
+    if (playTile.col < boardSetting.cols && playTile.row > 1) {
+      // iterate to check all lower rows. loop until hitting the bottom.
+      for (var i = 1; i < playTile.row + 1; i++) {
+        Tile tile = Tile(col: playTile.col + i, row: playTile.row - i);
+        if (takenTiles.contains(tile)) {
+          tempWinTiles.add(tile);
+        } else {
+          break;
+        }
+      }
+    }
+
+    // see if tempWinTiles meets the win condition, if so it's a win
+    if (tempWinTiles.length >= boardSetting.winCondition()) {
+      return tempWinTiles;
+    }
+
+    return null;
+  }
 
 }
